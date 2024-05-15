ceb596ca784ef2eb171f95ecaddac511e6de6f2a
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-05-15 06:03:51 +0300

    Disable inline diagnostics at the end of lines

--- init.lua
+++ init.lua
@@ -796,6 +796,10 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
+
+      vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+      })
     end,
   },
 
