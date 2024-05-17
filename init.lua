39f2978967468743989987c7b15754c7ade7390e
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-05-18 00:37:33 +0300

    Set up minimal startpage using mini.nvim

--- init.lua
+++ init.lua
@@ -757,6 +757,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- Minimal startpage
+      require('mini.starter').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
