ba7e1ae69629d4c21642e05eab1ec75f298e0589
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-05-22 21:18:19 +0300

    Add yaml, go and rust formatters to conform config

--- init.lua
+++ init.lua
@@ -586,11 +586,10 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
+        go = { 'gofmt' },
+        rust = { 'rustfmt' },
+
         javascript = { { 'prettierd', 'prettier' } },
         typescript = { { 'prettierd', 'prettier' } },
         html = { { 'prettierd', 'prettier' } },
@@ -598,6 +597,7 @@ require('lazy').setup({
         scss = { { 'prettierd', 'prettier' } },
         json = { { 'prettierd', 'prettier' } },
         jsonc = { { 'prettierd', 'prettier' } },
+        yaml = { { 'prettierd', 'prettier' } },
       },
     },
   },
