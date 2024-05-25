1d638884fff32d451e9502680becfaae6141249b
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-05-25 23:48:23 +0300

    Enable line-wrap automatically only for markdown filetype

--- init.lua
+++ init.lua
@@ -132,6 +132,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  command = 'set wrap',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
