6571ac81123dfbf39a01f4a93ecf3f27655b968a
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-08-27 04:02:54 +0300

    List required language servers for mason-tool-installer

--- init.lua
+++ init.lua
@@ -597,30 +597,23 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        ansiblels = {},
+        docker_compose_language_service = {},
+        dockerls = {},
+        gopls = {},
+        pyright = {},
+        tsserver = {},
+        shellcheck = {},
+        bashls = {},
+        shfmt = {},
+        hyprls = {},
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
