0ed24deadab3043843b83828912e9fb192b6d15a
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-11-08 15:23:58 +0300

    Create custom html.lua plugin with nvim-ts-autotag and nvim-emmet

---
+++ lua/custom/plugins/html.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+
+  {
+    'olrtg/nvim-emmet',
+    config = function()
+      vim.keymap.set({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+    end,
+  },
+}
