7a48767e5167b212aa36360592003e4367c10a9a
bayramkzk <48856944+bayramkzk@users.noreply.github.com>
2024-12-30 15:54:59 +0300

    Configure neovide gui font

--- init.lua
+++ init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.o.guifont = 'IosevkaTermSlab Nerd Font Mono:h14'
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
